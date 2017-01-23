0df9226e2eddedac97319893fa9682b4e3045034
Pradeep Dasigi <pdasigi@cs.cmu.edu>
2017-01-23 13:09:03 -0500

    (Almost) complete wikitables type declaration

--- src/main/java/org/allenai/wikitables/WikiTablesTypeDeclaration.java
+++ src/main/java/org/allenai/wikitables/WikiTablesTypeDeclaration.java
@@ -14,13 +14,24 @@ public class WikiTablesTypeDeclaration extends AbstractTypeDeclaration {
 
     // Atomic types
     public static final Type NUMBER_TYPE = Type.createAtomic("i");
-    // fb:row
+    public static final Type DATE_TYPE = Type.createAtomic("d");
+    public static final Type ENTIY_TYPE = Type.createAtomic("e");
+    public static final String ROW_TYPE_STRING = "fb:type.row";
     public static final Type ROW_TYPE = Type.createAtomic("r");
-    // Cells and parts can be expressed directly with the prefix.
+    public static final String CELL_TYPE_STRING = "fb:type.cell";
     public static final String CELL_PREFIX = "fb:cell";
     public static final Type CELL_TYPE = Type.createAtomic("c");
+    public static final String PART_TYPE_STRING = "fb:type.part";
     public static final String PART_PREFIX = "fb:part";  
     public static final Type PART_TYPE = Type.createAtomic("p");
+    public static final String EXPLICIT_FUNCTIONAL_STRING = "fb:type.object.type";
+
+    public static final Map<String, Type> EXPLICIT_TYPES = new HashMap<>();
+    static {
+        EXPLICIT_TYPES.put(ROW_TYPE_STRING, ROW_TYPE);
+        EXPLICIT_TYPES.put(CELL_TYPE_STRING, CELL_TYPE);
+        EXPLICIT_TYPES.put(PART_TYPE_STRING, PART_TYPE);
+    }
 
     // Functional types
     // Take a row and get its next row
@@ -32,11 +43,11 @@ public class WikiTablesTypeDeclaration extends AbstractTypeDeclaration {
     public static final Type ROW_INDEX_FUNCTION = Type.createFunctional(NUMBER_TYPE, ROW_TYPE, false);
 
     // Take a number and return a cell
-    // Note that we are using the same function type for dates as well.
     public static final String CELL_NUMBER_STRING = "fb:cell.cell.number";
-    public static final String CELL_DATE_STRING = "fb:cell.cell.date";
     public static final String CELL_NUM2_STRING = "fb:cell.cell.num2";
     public static final Type CELL_NUMBER_FUNCTION = Type.createFunctional(NUMBER_TYPE, CELL_TYPE, false);
+    public static final String CELL_DATE_STRING = "fb:cell.cell.date";
+    public static final Type CELL_DATE_FUNCTION = Type.createFunctional(DATE_TYPE, CELL_TYPE, false);
 
     // Take a string (part of a cell) and return a cell
     public static final String CELL_STR1_STRING = "fb:cell.cell.str1";
@@ -49,7 +60,7 @@ public class WikiTablesTypeDeclaration extends AbstractTypeDeclaration {
         FULLY_SPECIFIED_FUNCTIONS.put(ROW_NEXT_STRING, ROW_NEXT_FUNCTION);
         FULLY_SPECIFIED_FUNCTIONS.put(ROW_INDEX_STRING, ROW_INDEX_FUNCTION);
         FULLY_SPECIFIED_FUNCTIONS.put(CELL_NUMBER_STRING, CELL_NUMBER_FUNCTION);
-        FULLY_SPECIFIED_FUNCTIONS.put(CELL_DATE_STRING, CELL_NUMBER_FUNCTION);
+        FULLY_SPECIFIED_FUNCTIONS.put(CELL_DATE_STRING, CELL_DATE_FUNCTION);
         FULLY_SPECIFIED_FUNCTIONS.put(CELL_NUM2_STRING, CELL_NUMBER_FUNCTION);
         FULLY_SPECIFIED_FUNCTIONS.put(CELL_STR1_STRING, CELL_PART_FUNCTION);
         FULLY_SPECIFIED_FUNCTIONS.put(CELL_STR2_STRING, CELL_PART_FUNCTION);
@@ -60,30 +71,51 @@ public class WikiTablesTypeDeclaration extends AbstractTypeDeclaration {
     public static final String ROW_FUNCTION_PREFIX = "fb:row.row";
     public static final Type ROW_CELL_FUNCTION = Type.createFunctional(CELL_TYPE, ROW_TYPE, false);
 
+    public static final Map<String, String> SUPERTYPE_MAP = new HashMap<>();
+    static {
+        SUPERTYPE_MAP.put("r", "e");
+        SUPERTYPE_MAP.put("c", "e");
+        SUPERTYPE_MAP.put("p", "e");
+    }
+
     public WikiTablesTypeDeclaration(Map<String, String> supertypeMap) {
         super(supertypeMap);
     }
 
     public WikiTablesTypeDeclaration() {
-        this(new HashMap<String, String>());
+        this(SUPERTYPE_MAP);
     }
 
     @Override
     public Type getType(String constant) {
         if (FULLY_SPECIFIED_FUNCTIONS.containsKey(constant))
             return FULLY_SPECIFIED_FUNCTIONS.get(constant);
+        else if (EXPLICIT_TYPES.containsKey(constant))
+            return EXPLICIT_TYPES.get(constant);
         else if (constant.startsWith(CELL_PREFIX))
             return CELL_TYPE;
         else if (constant.startsWith(PART_PREFIX))
             return PART_TYPE;
         else if (constant.startsWith(ROW_FUNCTION_PREFIX))
             return ROW_CELL_FUNCTION;
-        else if (constant.matches("\\d*\\.?\\d+"))
+        else if (constant.matches("[-+]?\\d*\\.?\\d+"))
             return NUMBER_TYPE;
         else if (constant.equals("number"))
             return Type.createFunctional(NUMBER_TYPE, NUMBER_TYPE, false);
-        else if (constant.equalsIgnoreCase("reverse"))
+        else if (constant.equals("date"))
+            return Type.createFunctional(NUMBER_TYPE, DATE_TYPE, true);
+        else if (constant.equals("var") || constant.equals(EXPLICIT_FUNCTIONAL_STRING))
+            return Type.parseFrom("<#1,#1>");
+        else if (constant.equals("argmax") || constant.equals("argmin") || constant.equals("max") || constant.equals("min"))
+            return Type.createFunctional(NUMBER_TYPE, NUMBER_TYPE, true);
+        else if (constant.equals("count"))
+            return Type.parseFrom("<e,i>");
+        else if (constant.equals("reverse"))
             return Type.parseFrom("<<#2,#1>,<#1,#2>>");
+        else if (constant.equals("or") || constant.equals("and"))
+            return Type.parseFrom("<<#1,#1>,#1>");
+        else if (constant.equals("-"))
+            return Type.parseFrom("<<i,i>,i>");
         // Return atomic functions and the one prefix function.
         return TypeDeclaration.TOP;
     }
