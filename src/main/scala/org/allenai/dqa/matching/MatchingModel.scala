89c5b2f1f6c5f3bbbc3e384302532cfdae7b5a55
Joel Grus <joelgrus@gmail.com>
2017-03-10 11:23:55 -0800

    rename RichNumeric

--- src/main/scala/org/allenai/dqa/matching/MatchingModel.scala
+++ src/main/scala/org/allenai/dqa/matching/MatchingModel.scala
@@ -7,7 +7,7 @@ import org.allenai.pnp.Pnp.computationGraph
 import org.allenai.pnp.PnpModel
 
 import edu.cmu.dynet._
-import Utilities.RichNumeric
+import Utilities.NumbersAsExpressions
 
 import org.allenai.pnp.ExecutionScore
 import com.google.common.base.Preconditions
