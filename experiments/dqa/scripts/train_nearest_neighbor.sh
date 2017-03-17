8b5b8a5e1ab97a36b42de5aad2b7a41f4c013861
Jayant Krishnamurthy <jayantk@allenai.org>
2017-03-16 22:19:27 -0700

    training scripts

---
+++ experiments/dqa/scripts/train_nearest_neighbor.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+source "experiments/dqa/scripts/config.sh"
+
+MY_NAME=nearest_neighbor
+MY_DIR=$EXPERIMENT_DIR/$MY_NAME/
+MY_MODEL=$MY_DIR/model.ser
+MY_FLAGS="--nearestNeighbor --matchIndependent"
+
+mkdir -p $MY_DIR
+
+echo "Training $MY_NAME model..."
+./experiments/dqa/scripts/run.sh org.allenai.dqa.matching.TrainMatchingCli --beamSize 1 --epochs 0 --examples $TRAIN --diagrams $DIAGRAMS --diagramFeatures $DIAGRAM_FEATURES --modelOut $MY_MODEL $MY_FLAGS > $MY_DIR/log.txt
+
+echo "Testing $MY_NAME model..."
+./experiments/dqa/scripts/run.sh org.allenai.dqa.matching.TestMatchingCli --beamSize $TEST_BEAM --examples $TEST --diagrams $DIAGRAMS --diagramFeatures $DIAGRAM_FEATURES --model $MY_MODEL --lossJson $MY_DIR/validation_error_independent.json  > $MY_DIR/validation_error_independent_log.txt
+
+./experiments/dqa/scripts/run.sh org.allenai.dqa.matching.TestMatchingCli --beamSize 120 --enforceMatching --examples $TEST --diagrams $DIAGRAMS --diagramFeatures $DIAGRAM_FEATURES --model $MY_MODEL --lossJson $MY_DIR/validation_error_matching.json  > $MY_DIR/validation_error_matching_log.txt
+
+# mkdir -p $MY_DIR/validation_error/
+# python experiments/dqa/scripts/visualize_loss.py $MY_DIR/validation_error.json $MY_DIR/validation_error/
+# tar cf $MY_DIR/validation_error.tar $MY_DIR/validation_error/
+# gzip -f $MY_DIR/validation_error.tar
+
+# ./experiments/dqa/scripts/run.sh org.allenai.dqa.matching.TestMatchingCli --beamSize $TEST_BEAM --examples $TRAIN --diagrams $DIAGRAMS --diagramFeatures $DIAGRAM_FEATURES --model $MY_MODEL --lossJson $MY_DIR/train_error.json  > $MY_DIR/train_error_log.txt
+
+# mkdir -p $MY_DIR/train_error/
+# python experiments/dqa/scripts/visualize_loss.py $MY_DIR/train_error.json $MY_DIR/train_error/
+# tar cf $MY_DIR/train_error.tar $MY_DIR/train_error/
+# gzip -f $MY_DIR/train_error.tar
+
+echo "Finished training $MY_NAME"
+
