4fadf278df9495c069156d93b2b3407e2fa3d01d
pdasigi <pdasigi@cs.cmu.edu>
2017-03-28 15:52:09 -0400

    More logical form conversion and some unit tests

--- src/main/scala/org/allenai/wikitables/WikiTablesSemanticParserCli.scala
+++ src/main/scala/org/allenai/wikitables/WikiTablesSemanticParserCli.scala
@@ -94,7 +94,8 @@ class WikiTablesSemanticParserCli extends AbstractCli() {
     
 
     // Eliminate those examples that Sempre did not find correct logical forms for.
-    val trainPreprocessed = trainingData.filter(!_.alternativeFormulas.isEmpty).map(
+    val logicalFormPresent = (ex: CustomExample) => !ex.alternativeFormulas.isEmpty || ex.targetFormula != null
+    val trainPreprocessed = trainingData.filter(logicalFormPresent).map(
         x => preprocessExample(x, vocab, simplifier, logicalFormParser, typeDeclaration))
     println("Found correct logical forms for " + trainPreprocessed.size + " training examples")
 
