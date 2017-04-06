f47276a832a0cb9aa3e144c17d76e157bce49deb
Jayant Krishnamurthy <jayantk@jayantk3.dev.ai2>
2017-04-06 15:03:47 +0000

    config fiddling

--- experiments/wikitables/scripts/config.sh
+++ experiments/wikitables/scripts/config.sh
@@ -2,23 +2,24 @@
 
 SCRIPT_DIR="experiments/wikitables/scripts/"
 # Training data.
-TRAIN="data/WikiTableQuestions/data/subsamples/random-split_1-train_100.examples"
-# TRAIN="data/WikiTableQuestions/data/random-split-1-train.examples"
+# TRAIN="data/WikiTableQuestions/data/subsamples/random-split_1-train_100.examples"
+TRAIN="data/WikiTableQuestions/data/random-split-1-train.examples"
 # Development data used for evaluating model accuracy as training progresses.
-# TRAIN_DEV="data/WikiTableQuestions/data/subsamples/random-split_1-dev_500.examples"
-TRAIN_DEV="data/WikiTableQuestions/data/subsamples/random-split_1-dev_100.examples"
+TRAIN_DEV="data/WikiTableQuestions/data/subsamples/random-split_1-dev_500.examples"
+# TRAIN_DEV="data/WikiTableQuestions/data/subsamples/random-split_1-dev_100.examples"
 # Development data for evaluating the final trained model.
 # DEV="data/WikiTableQuestions/data/subsamples/random-split_1-dev_1000.examples"
 DEV="data/WikiTableQuestions/data/random-split-1-dev.examples"
 DERIVATIONS_PATH="data/wikitables/dpd_output/onedir2"
 
-EXPERIMENT_NAME="lstm_03_"
+EXPERIMENT_NAME="ll_200_beam_10"
 EXPERIMENT_DIR="experiments/wikitables/output/$EXPERIMENT_NAME/"
 
 EPOCHS=20
-MAX_TRAINING_DERIVATIONS=1
+MAX_TRAINING_DERIVATIONS=200
 MAX_TEST_DERIVATIONS=10
-BEAM_SIZE=1
+BEAM_SIZE=10
+TEST_BEAM_SIZE=10
 VOCAB=2
 
 # Layer dimensionalities of semantic parser
