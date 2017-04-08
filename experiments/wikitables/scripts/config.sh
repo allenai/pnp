c7be79401df0779602b762b449208af97fe21411
Jayant Krishnamurthy <jayantk@jayantk4.dev.ai2>
2017-04-08 17:18:06 +0000

    config for experiments

--- experiments/wikitables/scripts/config.sh
+++ experiments/wikitables/scripts/config.sh
@@ -3,20 +3,20 @@
 SCRIPT_DIR="experiments/wikitables/scripts/"
 # Training data.
 # TRAIN="data/WikiTableQuestions/data/subsamples/random-split_1-train_100.examples"
-TRAIN="data/WikiTableQuestions/data/random-split-1-train.examples"
+TRAIN="data/WikiTableQuestions/data/random-split-5-train.examples"
 # Development data used for evaluating model accuracy as training progresses.
 # TRAIN_DEV="data/WikiTableQuestions/data/subsamples/random-split_1-dev_500.examples"
 # TRAIN_DEV="data/WikiTableQuestions/data/subsamples/random-split_1-dev_100.examples"
-TRAIN_DEV="data/WikiTableQuestions/data/random-split-1-dev.examples"
+TRAIN_DEV="data/WikiTableQuestions/data/random-split-5-dev.examples"
 # Development data for evaluating the final trained model.
 # DEV="data/WikiTableQuestions/data/subsamples/random-split_1-dev_1000.examples"
-DEV="data/WikiTableQuestions/data/random-split-1-dev.examples"
+DEV="data/WikiTableQuestions/data/random-split-5-dev.examples"
 # DEV="data/WikiTableQuestions/data/subsamples/random-split_1-dev_500.examples"
 DERIVATIONS_PATH="data/wikitables/dpd_output/onedir2"
 # WORD_EMBEDDINGS="data/wikitables/glove.6B.200d.txt"
 
-EXPERIMENT_NAME="fold1"
-EXPERIMENT_DIR="experiments/wikitables/output/$EXPERIMENT_NAME/"
+EXPERIMENT_NAME="fold5"
+EXPERIMENT_DIR="experiments/wikitables/output/dev_folds/$EXPERIMENT_NAME/"
 
 EPOCHS=20
 MAX_TRAINING_DERIVATIONS=100
